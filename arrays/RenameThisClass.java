a7185a76ae599d50ecab4c60d1765c8ce605b794
EnriquedeCastro <143217608+EnriquedeCastro@users.noreply.github.com>
2023-09-15 11:34:59 -0700

    Add files via upload

---
+++ arrays/RenameThisClass.java
@@ -0,0 +1,29 @@
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RenameThisClass
+{
+    public static void main(String[] args) {
+       String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
+    for (int i = 0; i < strArray.length; i++){
+            System.out.println(strArray[i]);
+    }
+    for (String value : strArray) {
+        System.out.print(value);
+    }
+    for (int i = 4; i < 5; i++) {
+        System.out.println("  ");
+    }
+    for (int i = 0; i < strArray.length; i++) {
+        if (i == strArray.length-1) {
+            System.out.print(strArray[i]);
+            break;
+        }
+        else
+        System.out.print(strArray[i] + "-");
+        }
+    }
+}
